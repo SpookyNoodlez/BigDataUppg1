bec543e2358334c1e376f5ade070af17594c3a51
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2021-01-24 00:24:18 +0200

    Uppgift 1 och 2 klara

---
+++ uppg1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 19:44:16 2021
+
+@author: joann
+"""
+
+print("Vilken tabell?")
+x = int(input())
+
+for i in range(10):
+    j = i + 1
+    result = x * j
+    print(result)
+    
---
+++ uppg2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 19:58:44 2021
+
+@author: joann
+"""
+
+print("Ange tal")
+x = input()
+x = int(x)
+
+print(x)
+if(x % 2 == 0):
+    print("är ett jämt tal")
+else:
+    print("är ett udda tal")
